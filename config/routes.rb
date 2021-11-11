af38035beb3fb0cdb09b52d4473a238431ed395a
EC2 Default User <ec2-user@ip-172-31-68-172.ec2.internal>
2021-11-11 14:27:51 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   
   resources :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  	resources :book_comments, only: [:create, :destroy]
+  end
 end
